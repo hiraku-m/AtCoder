2acbfb17855c1c2f59fdeb4fb2e43187e6f3b701
hiraku-m <hiraku.morita@gmail.com>
2020-07-29 17:52:04 +0900

    Wrote ABC117_B

---
+++ ABC/ABC117/ABC117_B.py
@@ -0,0 +1,46 @@
+import sys
+from io import StringIO
+import unittest
+
+def resolve():
+    input()
+    L = list(map(int, input().split()))
+    M = max(L)
+    R = sum(L) - M
+    if R > M:
+        print('Yes')
+    else:
+        print('No')
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """4
+3 8 5 1"""
+        output = """Yes"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """4
+3 8 4 1"""
+        output = """No"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """10
+1 8 10 5 8 12 34 100 11 3"""
+        output = """No"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
