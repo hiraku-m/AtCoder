03a0afee36c95cb82a779391ad25cd667157990f
hiraku-m <hiraku.morita@gmail.com>
2020-08-01 20:04:14 +0900

    Wrote ABC120_A

--- ABC/ABC120/ABC120_A.py
+++ ABC/ABC120/ABC120_A.py
@@ -3,7 +3,8 @@ from io import StringIO
 import unittest
 
 def resolve():
-
+    a, b, c = map(int, input().split())
+    print(min(b//a, c))
 
 class TestClass(unittest.TestCase):
     def assertIO(self, input, output):
@@ -16,49 +17,18 @@ class TestClass(unittest.TestCase):
         self.assertEqual(out, output)
 
     def test_入力例_1(self):
-        input = """84 97 66
-79 89 11
-61 59 7
-7
-89
-7
-87
-79
-24
-84
-30"""
-        output = """Yes"""
+        input = """2 11 4"""
+        output = """4"""
         self.assertIO(input, output)
 
     def test_入力例_2(self):
-        input = """41 7 46
-26 89 2
-78 92 8
-5
-6
-45
-16
-57
-17"""
-        output = """No"""
+        input = """3 9 5"""
+        output = """3"""
         self.assertIO(input, output)
 
     def test_入力例_3(self):
-        input = """60 88 34
-92 41 43
-65 73 48
-10
-60
-43
-88
-11
-48
-73
-65
-41
-92
-34"""
-        output = """Yes"""
+        input = """100 1 10"""
+        output = """0"""
         self.assertIO(input, output)
 
 
